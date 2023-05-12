77b7fbe9c1de0cfcd8fa27eda858d5c7e0f3cb6a
skylab22 <santosericalmeida92@gmail.com>
2023-05-12 11:27:56 -0300

    Atual

--- script.js
+++ script.js
@@ -1,103 +1,118 @@
 function acucar(){
-    acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Super Market no valor de 3,87R$'
+    acucarmaisbarato.innerHTML = 'O açucar mais barato está no mercado Rio Sul no valor de 3,99R$'
+    economiaacucar.innerHTML = ''
     acucarredeeconomia.innerHTML = 'Rede Economia não tem promoção'
-    acucarriosul.innerHTML = 'Rio Sul não tem promoção'
-    acucarsupermarket.innerHTML = 'Super Market Açucar refinado união: 3,87R$'
+    acucarriosul.innerHTML = 'Rio Sul Açucar refinado caravelas: 3,99R$'
+    acucarsupermarket.innerHTML = 'Super Market não tem promoção'
     acucarsuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function arroz(){
-    arrozmaisbarato.innerHTML = 'O arroz mais barato está no mercado Rede Economia no valor de 22,95R$'
+    arrozmaisbarato.innerHTML = 'O arroz mais barato está no mercado Super Market no valor de 19,95R$'
+    economiaarroz.innerHTML = 'Economizou 3,00R$ em relação ao mercado mais caro.'
     arrozredeeconomia.innerHTML = 'Rede Economia Arroz branco Palmares: 22,95R$'
-    arrozriosul.innerHTML = 'Rio Sul não tem promoção'
-    arrozsupermarket.innerHTML = 'Super Market não tem promoção'
-    arrozsuperrede.innerHTML = 'Super Rede não tem promoção'
+    arrozriosul.innerHTML = 'Rio Sul Arroz branco tucano: 19,99R$'
+    arrozsupermarket.innerHTML = 'Super Market Arroz branco donal elza: 19,95R$'
+    arrozsuperrede.innerHTML = 'Super Rede Arroz não tem promoção'
 }
 function batatafrita(){
-    batatafritamaisbarato.innerHTML = 'A batata frita mais barata está no mercado Super Rede no valor de 6,99R$'
+    batatafritamaisbarato.innerHTML = 'Até o momento não tem detergente em promoção'
+    economiabatatafrita.innerHTML = ''
     batatafritaredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     batatafritariosul.innerHTML = 'Rio Sul não tem promoção'
     batatafritasupermarket.innerHTML = 'Super Market não tem promoção'
-    batatafritasuperrede.innerHTML = 'Super Rede batata frita quality fries: 6,99R$'
+    batatafritasuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function cafe(){
     cafemaisbarato.innerHTML = 'O café mais barato está no mercado Rede Economia no valor de 13,98R$'
+    economiacafe.innerHTML = 'Economizou 3,00R$ em relação ao mercado mais caro.'
     caferedeeconomia.innerHTML = 'Rede Economia café tá bem extra forte: 13,98R$'
-    caferiosul.innerHTML = 'Rio Sul não tem promoção'
-    cafesupermarket.innerHTML = 'Super Market café melitta extra forte: 16,98R$'
+    caferiosul.innerHTML = 'Rio Sul café evolutto extra forte: 14,99R$'
+    cafesupermarket.innerHTML = 'Super Market café pilão extra forte: 16,98R$'
     cafesuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function carne(){
-    carnemaisbarato.innerHTML = 'A carne mais barata está no mercado Super Rede no valor de 8,95R$'
+    carnemaisbarato.innerHTML = 'A carne mais barata está no mercado Super Market no valor de 13,97R$'
+    economiacarne.innerHTML = 'Economizou 0,02R$ em relação ao mercado mais caro.'
     carneredeeconomia.innerHTML = 'Rede Economia não tem promoção'
-    carneriosul.innerHTML = 'Rio Sul não tem promoção'
-    carnesupermarket.innerHTML = 'Super Market Alcatra bovina peça inteira 1 quilograma: 29,95R$ e Acém bovino peça 1kg: 23,95R$'
-    carnesuperrede.innerHTML = 'Super Rede Acém peça 1kg: 23,95R$ Mocotó bovino 1kg: 8,95R$ Carré suíno 1Kg: 13,95R$ Chispe/Garganta/Orelha salgada 1Kg: 9,98R$ Costela Salgada/Lombo 1Kg: 18,95R$'
+    carneriosul.innerHTML = 'Rio Sul pernill suino com osso e com pele 1kg: 13,99R$'
+    carnesupermarket.innerHTML = 'Super Market carré suíno 1kg: 13,97R$'
+    carnesuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function cerveja(){
-    cervejamaisbarato.innerHTML = 'A cerveja mais barata está no mercado Rede Economia no valor de 2,79R$'
-    cervejaredeeconomia.innerHTML = 'Rede Economia Cerveja Budweiser: 2,79R$ e Cerveja Heineken: 3,99R$'
-    cervejariosul.innerHTML = 'Rio Sul não tem promoção'
+    cervejamaisbarato.innerHTML = 'A cerveja mais barata está no mercado Rio Sul no valor de 2,59R$'
+    economiacerveja.innerHTML = 'Economizou 1,40R$ em relação ao mercado mais caro.'
+    cervejaredeeconomia.innerHTML = 'Rede Economia cerveja budweiser: 2,79R$ e cerveja heineken: 3,99R$'
+    cervejariosul.innerHTML = 'Rio Sul cerveja pilsen: 2,59R$'
     cervejasupermarket.innerHTML = 'Super Market não tem promoção'
-    cervejasuperrede.innerHTML = 'Super Rede Cerveja Itaipava: 3,39R$'
+    cervejasuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function cremedeleite(){//Talvez possa ser retirado
     cremedeleitemaisbarato.innerHTML = 'Até o momento não tem creme de leite em promoção'
+    economiacremedeleite.innerHTML = ''
     cremedeleiteredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     cremedeleiteriosul.innerHTML = 'Rio Sul não tem promoção'
     cremedeleitesupermarket.innerHTML = 'Super Market não tem promoção'
     cremedeleitesuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function detergente(){//Talvez possa ser retirado
-    detergentemaisbarato.innerHTML = 'Até o momento não tem detergente em promoção'
+    detergentemaisbarato.innerHTML = 'O detergente mais barato está no mercado Super Rede no valor de 1,59R$'
+    economiadetergente.innerHTML = ''
     detergenteredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     detergenteriosul.innerHTML = 'Rio Sul não tem promoção'
     detergentesupermarket.innerHTML = 'Super Market não tem promoção'
-    detergentesuperrede.innerHTML = 'Super Rede não tem promoção'
+    detergentesuperrede.innerHTML = 'Super Rede detergente barra: 1,59R$'
 }
 function feijao(){
-    feijaomaisbarato.innerHTML = 'O feijão mais barato está no mercado Rede Economia no valor de 6,99R$'
+    feijaomaisbarato.innerHTML = 'O feijão mais barato está no mercado Rede Economia/Rio Sul no valor de 6,99R$'
+    economiafeijao.innerHTML = ''
     feijaoredeeconomia.innerHTML = 'Rede Economia Feijão Malú: 6,99R$'
-    feijaoriosul.innerHTML = 'Rio Sul não tem promoção'
-    feijaosupermarket.innerHTML = 'Super Market Feijão sabor máximo: 7,89R$'
+    feijaoriosul.innerHTML = 'Rio Sul Feijão panela de barro: 6,99R$'
+    feijaosupermarket.innerHTML = 'Super Market não tem promoção'
     feijaosuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function frango(){
     frangomaisbarato.innerHTML = 'O frango mais barato está no mercado Rede Economia no valor de 5,97R$'
-    frangoredeeconomia.innerHTML = 'Rede Economia Coxa com sobrecoxa de frango 1 quilo: 5,97R$ e Filezinho de peito de frango 1 quilo: 12,97R$'
+    economiafrango.innerHTML = ''
+    frangoredeeconomia.innerHTML = 'Rede Economia coxa com sobrecoxa de frango 1kg: 5,97R$ e filezinho de peito de frango 1kg: 12,97R$'
     frangoriosul.innerHTML = 'Rio Sul não tem promoção'
     frangosupermarket.innerHTML = 'Super Market não tem promoção'
     frangosuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function leiteemcaixa(){
     leiteemcaixamaisbarato.innerHTML = 'O leite em caixa mais barato está no mercado Super Market no valor de 4,77R$'
+    economialeiteemcaixa.innerHTML = 'Economizou 1,72R$ em relação ao mercado mais caro.'
     leiteemcaixaredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     leiteemcaixariosul.innerHTML = 'Rio Sul não tem promoção'
-    leiteemcaixasupermarket.innerHTML = 'Super Market Leite longa vida quatá 1litro: 4,77R$'
-    leiteemcaixasuperrede.innerHTML = 'Super Rede não tem promoção'
+    leiteemcaixasupermarket.innerHTML = 'Super Market Leite longa vida quatá ou godam 1litro: 4,77R$'
+    leiteemcaixasuperrede.innerHTML = 'Super Rede leite piracanjuba 1litro: 6,49R$'
 }
 function leiteempo(){//Talvez possa ser retirado
-    leiteempomaisbarato.innerHTML = 'Até o momento não tem leite em pó em promoção'
+    leiteempomaisbarato.innerHTML = 'O leite em pó está no mercado Super Rede no valor de 13,95R$'
+    economialeiteempo.innerHTML = 'Economizou 1,03R$ em relação ao mercado mais caro.'
     leiteemporedeeconomia.innerHTML = 'Rede Economia não tem promoção'
     leiteemporiosul.innerHTML = 'Rio Sul não tem promoção'
-    leiteemposupermarket.innerHTML = 'Super Market não tem promoção'
-    leiteemposuperrede.innerHTML = 'Super Rede não tem promoção'
+    leiteemposupermarket.innerHTML = 'Super Market leite em pó ninho: 14,98R$'
+    leiteemposuperrede.innerHTML = 'Super Rede leite em pó fazendas do rio no valor de: 13,95R$'
 }
 function macarrao(){
-    macarraomaisbarato.innerHTML = 'O macarrão mais barato está no mercado Super Market no valor de 5,98R$'
+    macarraomaisbarato.innerHTML = 'O macarrão mais barato está no mercado Rede Economia no valor de 5,98R$'
+    economiamacarrao.innerHTML = ''
     macarraoredeeconomia.innerHTML = 'Rede Economia Macarrão cadore: 5,98R$'
     macarraoriosul.innerHTML = 'Rio Sul não tem promoção'
     macarraosupermarket.innerHTML = 'Super Market não tem promoção'
     macarraosuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function margarina(){
-    margarinamaisbarato.innerHTML = 'A margarina mais barata está no mercado Super Market no valor de 4,97R$'
-    margarinaredeeconomia.innerHTML = 'Rede Economia Margarina Claybom: 6,99R$'
+    margarinamaisbarato.innerHTML = 'A margarina mais barata está no mercado Rede Economia no valor de 6,99R$'
+    economiamargarina.innerHTML = 'Economizou 6,96R$ em relação ao mercado mais caro.'
+    margarinaredeeconomia.innerHTML = 'Rede Economia margarina claybom 500g: 6,99R$'
     margarinariosul.innerHTML = 'Rio Sul não tem promoção'
-    margarinasupermarket.innerHTML = 'Super Market Margarina Claybom: 4,97R$'
-    margarinasuperrede.innerHTML = 'Super Rede não tem promoção'
+    margarinasupermarket.innerHTML = 'Super Market não tem promoção'
+    margarinasuperrede.innerHTML = 'Super Rede margarina claybom 1kg: 13,95R$'
 }
 function molhodetomate(){//Talvez possa ser retirado
     molhodetomatemaisbarato.innerHTML = 'Até o momento não tem molho de tomate em promoção'
+    economiamolhodetomate.innerHTML = ''
     molhodetomateredeeconomia.innerHTML = 'Rede Economia não tem promoção'
     molhodetomateriosul.innerHTML = 'Rio Sul não tem promoção'
     molhodetomatesupermarket.innerHTML = 'Super Market não tem promoção'
@@ -105,33 +120,27 @@ function molhodetomate(){//Talvez possa ser retirado
 }
 function oleo(){
     oleomaisbarato.innerHTML = 'O óleo mais barato está no mercado Super Market no valor de 4,99R$'
+    economiaoleo.innerHTML = 'Economizou 1,00R$ em relação ao mercado mais caro.'
     oleoredeeconomia.innerHTML = 'Rede Economia oléo de soja lisa: 5,99R$'
     oleoriosul.innerHTML = 'Rio Sul não tem promoção'
-    oleosupermarket.innerHTML = 'Super Market oléo de soja lisa: 4,99R$'
-    oleosuperrede.innerHTML = 'Super Rede oléo de soja lisa: 6,99R$'
-}
-function refrigerante(){//Talvez possa ser retirado
-    refrigerantemaisbarato.innerHTML = 'Até o momento não tem refrigerante em promoção'
-    refrigeranteredeeconomia.innerHTML = 'Rede Economia não tem promoção'
-    refrigeranteriosul.innerHTML = 'Rio Sul não tem promoção'
-    refrigerantesupermarket.innerHTML = 'Super Market não tem promoção'
-    refrigerantesuperrede.innerHTML = 'Super Rede não tem promoção'
+    oleosupermarket.innerHTML = 'Super Market oléo de soja liza, soya ou leve: 4,99R$'
+    oleosuperrede.innerHTML = 'Super Rede não tem promoção'
 }
 function sabaoempo(){
-    sabaoempomaisbarato.innerHTML = 'O sabão em pó mais barato está no mercado Rede Economia no valor de 8,98R$'
+    sabaoempomaisbarato.innerHTML = 'O sabão em pó mais barato está no mercado Super Rede no valor de 8,95R$'
+    economiasabaoempo.innerHTML = 'Economizou 5,03R$ em relação ao mercado mais caro.'
     sabaoemporedeeconomia.innerHTML = 'Rede Economia Sabão em pó astra: 8,98R$'
     sabaoemporiosul.innerHTML = 'Rio Sul não tem promoção'
-    sabaoemposupermarket.innerHTML = 'Super Market não tem promoção'
-    sabaoemposuperrede.innerHTML = 'Super Rede não tem promoção'
+    sabaoemposupermarket.innerHTML = 'Super Market sabão em pó surf: 13,98R$'
+    sabaoemposuperrede.innerHTML = 'Super Rede sabão em pó astra: 8,95R$'
 }
 function rank(){
     rankmaisbarato.innerHTML = 'A classificação segue do mercado mais barato para o mais caro'
-    rankmaisbarato1.innerHTML = '1) Rede Economia'
-    rankmaisbarato2.innerHTML = '2) Super Market'
-    rankmaisbarato3.innerHTML = '3) Super Rede'
-    rankmaisbarato4.innerHTML = '4) Rio Sul'
+    rankmaisbarato1.innerHTML = '1) Rede Economia com 5 produtos'
+    rankmaisbarato2.innerHTML = '2) Super Market com 4 produtos'
+    rankmaisbarato3.innerHTML = '3) Rio Sul com 3 produtos | Super Rede com 3 produtos'
+    rankmaisbarato4.innerHTML = ''
 }
 function problema(){
-    alert("Favor entrar em contato pelo e-mail: skilobaloba@gmail.com","aaa")
-}//Adcionar Ovo ao todo tem 18 produtos se tirar os 5 e adcionar o ovo ficaria 14
- //Adcionar botão do mercado mais barato no geral
+    alert("Entrar em contato pelo e-mail: skilobaloba@gmail.com")
+}//Adcionar Ovo ao todo tem 17 produtos se tirar os 4 e adcionar o ovo ficaria 13
