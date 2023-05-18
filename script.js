54e70e12f36b2f0af14f61d83c411ab20d5eea7c
skylab22 <santosericalmeida92@gmail.com>
2023-05-18 13:03:27 -0300

    Atual

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 function acucar(){
-    acucarmaisbarato.innerHTML = 'Até o momento não tem promoção'
+    acucarmaisbarato.innerHTML = 'Mercado mais barato Rio Sul: 3,99R$'
     economiaacucar.innerHTML = ''
-    acucarredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
-    acucarriosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
-    acucarsupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    acucarsuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    acucarredeeconomia.innerHTML = '<strong>Rede Economia </strong><br>Não tem promoção'
+    acucarriosul.innerHTML = '<strong>Rio Sul</strong><br>Último preço do açucar refinado caravelas: 3,99R$'
+    acucarsupermarket.innerHTML = '<strong>Super Market</strong><br>Não tem promoção'
+    acucarsuperrede.innerHTML = '<strong>Super Rede</strong><br>Não tem promoção'
 }
 function arroz(){
-    arrozmaisbarato.innerHTML = 'Mercado mais barato Super Market: 19,95R$'
-    economiaarroz.innerHTML = ''
-    arrozredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
-    arrozriosul.innerHTML = '<strong>Rio Sul</strong><br> arroz branco tucano: 19,99R$'
-    arrozsupermarket.innerHTML = '<strong>Super Market</strong><br> arroz branco dona elza: 19,95R$'
-    arrozsuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    arrozmaisbarato.innerHTML = 'Mercado mais barato Super Market: 19,95R$R$'
+    economiaarroz.innerHTML = 'Você economizou 2,00R$ em relação ao mercado mais caro'
+    arrozredeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Preço autal arroz branco Palmares: 21,95R$'
+    arrozriosul.innerHTML = '<strong>Rio Sul</strong><br>Preço atual arroz branco tucano: 19,99R$'
+    arrozsupermarket.innerHTML = '<strong>Super Market</strong><br>Preço atual arroz branco dona elza: 19,95R$'
+    arrozsuperrede.innerHTML = '<strong>Super Rede</strong><br>Não tem promoção'
 }
 function batatafrita(){
     batatafritamaisbarato.innerHTML = 'Até o momento não tem promoção'
     economiabatatafrita.innerHTML = ''
-    batatafritaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
-    batatafritariosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
-    batatafritasupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    batatafritasuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    batatafritaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Não tem promoção'
+    batatafritariosul.innerHTML = '<strong>Rio Sul</strong><br>Não tem promoção'
+    batatafritasupermarket.innerHTML = '<strong>Super Market</strong><br>Não tem promoção'
+    batatafritasuperrede.innerHTML = '<strong>Super Rede</strong><br>Não tem promoção'
 }
 function cafe(){
-    cafemaisbarato.innerHTML = 'Mercado mais barato Rio Sul: 14,99R$'
-    economiacafe.innerHTML = 'Você economizou 2,00R$ em relação ao mercado mais caro.'
-    caferedeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
-    caferiosul.innerHTML = '<strong>Rio Sul</strong><br> café evolutto extra forte: 14,99R$'
-    cafesupermarket.innerHTML = '<strong>Super Market</strong><br> café pilão extra forte: 16,98R$'
+    cafemaisbarato.innerHTML = 'Mercado mais barato Rede Economia: 13,98R$'
+    economiacafe.innerHTML = 'Você economizou 3,00R$ em relação ao mercado mais caro.'
+    caferedeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Preço atual café tá bem: 13,98R$'
+    caferiosul.innerHTML = '<strong>Rio Sul</strong><br>Preço atual café evolutto extra forte: 14,99R$'
+    cafesupermarket.innerHTML = '<strong>Super Market</strong><br>Preço atual café pilão extra forte: 16,98R$'
     cafesuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
 }
 function carne(){
-    carnemaisbarato.innerHTML = 'Mercado mais barato Rio Sul: 13,99R$'
+    carnemaisbarato.innerHTML = 'Mercado com carré suíno mais barato Super Rede: 13,95R$'
     economiacarne.innerHTML = ''
     carneredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
-    carneriosul.innerHTML = '<strong>Rio Sul</strong><br> pernill suino com osso e com pele 1kg: 13,99R$'
-    carnesupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    carnesuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    carneriosul.innerHTML = '<strong>Rio Sul</strong><br> pernill suíno com osso e com pele 1kg: 13,99R$'
+    carnesupermarket.innerHTML = '<strong>Super Market</strong><br> Preço atual filé de costela maturatta ou steakhouse montana 1kg: 39,98R$<br>Preço atual alcatra bovina 1kg: 29,95R$<br>Preço atual carré suíno 1kg: 13,97R$'
+    carnesuperrede.innerHTML = '<strong>Super Rede</strong><br> Preço atual pá ou acém 1kg: 26,95R$<br>Preço atual mocotó bovino 1kg: 10,98R$<br>Preço atual carré suíno 1kg: 13,95R$'
 }
 function cerveja(){
-    cervejamaisbarato.innerHTML = 'Mercado mais barato Rio Sul: 2,59R$'
-    economiacerveja.innerHTML = ''
-    cervejaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
-    cervejariosul.innerHTML = '<strong>Rio Sul</strong><br> cerveja pilsen: 2,59R$'
+    cervejamaisbarato.innerHTML = 'Mercado mais barato Rio Sul: R$'
+    economiacerveja.innerHTML = 'Você economizou 3,41R$ em relação ao mercado mais caro.'
+    cervejaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Preço atual cerveja corona extra: 5,99R$'
+    cervejariosul.innerHTML = '<strong>Rio Sul</strong><br>Preço atual cerveja pilsen: 2,59R$'
     cervejasupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    cervejasuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    cervejasuperrede.innerHTML = '<strong>Super Rede</strong><br>Preço atual cerveja itaipava: 3,99R$'
 }
 function cremedeleite(){//Talvez possa ser retirado
     cremedeleitemaisbarato.innerHTML = 'Até o momento não tem promoção'
@@ -63,52 +63,52 @@ function detergente(){//Talvez possa ser retirado
     detergentesuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
 }
 function feijao(){
-    feijaomaisbarato.innerHTML = 'Mercado mais barato Rio Sul: 6,99R$'
+    feijaomaisbarato.innerHTML = 'Mercado mais barato Rede Economia/Rio Sul: 6,99R$'
     economiafeijao.innerHTML = ''
-    feijaoredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
-    feijaoriosul.innerHTML = '<strong>Rio Sul</strong><br> Feijão panela de barro: 6,99R$'
-    feijaosupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    feijaosuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    feijaoredeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Preço atual feijão preto malú: 6,99R$'
+    feijaoriosul.innerHTML = '<strong>Rio Sul</strong><br>Preço atual feijão preto panela de barro: 6,99R$'
+    feijaosupermarket.innerHTML = '<strong>Super Market</strong><br>Preço atual feijão preto sabor máximo: 7,89R$'
+    feijaosuperrede.innerHTML = '<strong>Super Rede</strong><br>não tem promoção'
 }
 function frango(){
-    frangomaisbarato.innerHTML = 'Mercado mais barato Super Market: 12,97R$'
-    economiafrango.innerHTML = ''
+    frangomaisbarato.innerHTML = 'Mercado com filé de peito mais barato Super Market: 12,97R$'
+    economiafrango.innerHTML = 'Você economizou 3,03R$ em relação ao mercado mais caro.'
     frangoredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
     frangoriosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
-    frangosupermarket.innerHTML = '<strong>Super Market</strong><br> filé de peito 1kg: 12,97R$'
-    frangosuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
+    frangosupermarket.innerHTML = '<strong>Super Market</strong><br>Preço atual filé de peito 1kg: 12,97R$<br>Preço atual drumet 1kg: 9,97R$<br>Preço atual coxa com sobrecoxa 1kg: 5,97R$'
+    frangosuperrede.innerHTML = '<strong>Super Rede</strong><br>Preço atual filé de peito aurora bandeja 1kg: 15,99R$<br>Preço atual coxa de frango sadia 800g: 13,99R$'
 }
 function leiteemcaixa(){
     leiteemcaixamaisbarato.innerHTML = 'Mercado mais barato Super Market: 4,77R$'
     economialeiteemcaixa.innerHTML = 'Você economizou 1,72R$ em relação ao mercado mais caro'
     leiteemcaixaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
     leiteemcaixariosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
-    leiteemcaixasupermarket.innerHTML = '<strong>Super Market</strong><br> Leite longa vida quatá ou godam 1litro: 4,77R$'
-    leiteemcaixasuperrede.innerHTML = '<strong>Super Rede</strong><br> Leite piracanjuba 1litro: 6,49R$'
+    leiteemcaixasupermarket.innerHTML = '<strong>Super Market</strong><br>Preço atual leite longa vida quatá ou tirol 1litro: 4,77R$'
+    leiteemcaixasuperrede.innerHTML = '<strong>Super Rede</strong><br>Último preço leite piracanjuba 1litro: 6,49R$'
 }
 function leiteempo(){//Talvez possa ser retirado
     leiteempomaisbarato.innerHTML = 'Mercado mais barato Super Rede: 13,95R$'
-    economialeiteempo.innerHTML = 'Você economizou 1,00R$ em relação ao mercado mais caro.'
+    economialeiteempo.innerHTML = 'Você economizou 1,03R$ em relação ao mercado mais caro.'
     leiteemporedeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
     leiteemporiosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
-    leiteemposupermarket.innerHTML = '<strong>Super Market</strong><br> leite em pó ninho: 14,98R$'
-    leiteemposuperrede.innerHTML = '<strong>Super Rede</strong><br> leite em pó fazendas do rio: 13,95R$'
+    leiteemposupermarket.innerHTML = '<strong>Super Market</strong><br>Último preço leite em pó ninho: 14,98R$'
+    leiteemposuperrede.innerHTML = '<strong>Super Rede</strong><br>Último preço leite em pó fazendas do rio: 13,95R$'
 }
 function macarrao(){
-    macarraomaisbarato.innerHTML = 'Até o momento não tem detergente em promoção'
+    macarraomaisbarato.innerHTML = 'Mercado mais barato Rede Economia: 5,98R$'
     economiamacarrao.innerHTML = ''
-    macarraoredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
+    macarraoredeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Preço atual massa sêmola cadore 1kg: 5,98R$'
     macarraoriosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
     macarraosupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
     macarraosuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
 }
 function margarina(){
-    margarinamaisbarato.innerHTML = 'Mercado mais barato Super Rede: 7,99R$'
+    margarinamaisbarato.innerHTML = 'Mercado mais barato Rede Economia: 6,97R$'
     economiamargarina.innerHTML = ''
-    margarinaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
+    margarinaredeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Preço atual margarina becel 500g: 6,97R$<br>Preço atual margarina qualy: 8,99R$'
     margarinariosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
     margarinasupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    margarinasuperrede.innerHTML = '<strong>Super Rede</strong><br> manteiga kreminas 200g: 7,99R$'
+    margarinasuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
 }
 function molhodetomate(){//Talvez possa ser retirado
     molhodetomatemaisbarato.innerHTML = 'Até o momento não tem promoção'
@@ -123,23 +123,23 @@ function oleo(){
     economiaoleo.innerHTML = ''
     oleoredeeconomia.innerHTML = '<strong>Rede Economia</strong><br>não tem promoção'
     oleoriosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
-    oleosupermarket.innerHTML = '<strong>Super Market</strong><br> oléo de soja liza, soya ou leve: 4,99R$'
+    oleosupermarket.innerHTML = '<strong>Super Market</strong><br>Preço atual oléo de soja liza, soya ou leve: 4,99R$'
     oleosuperrede.innerHTML = '<strong>Super Rede</strong><br> não tem promoção'
 }
 function sabaoempo(){
-    sabaoempomaisbarato.innerHTML = 'Mercado mais barato Super Rede: 15,95R$'
-    economiasabaoempo.innerHTML = ''
-    sabaoemporedeeconomia.innerHTML = '<strong>Rede Economia</strong><br> não tem promoção'
+    sabaoempomaisbarato.innerHTML = 'Mercado mais barato Rede Economia: 8,98R$'
+    economiasabaoempo.innerHTML = 'Você economizou 6,97R$ em relação ao mercado mais caro.'
+    sabaoemporedeeconomia.innerHTML = '<strong>Rede Economia</strong><br>Preço atual sabão em pó astra: 8,98R$'
     sabaoemporiosul.innerHTML = '<strong>Rio Sul</strong><br> não tem promoção'
     sabaoemposupermarket.innerHTML = '<strong>Super Market</strong><br> não tem promoção'
-    sabaoemposuperrede.innerHTML = '<strong>Super Rede</strong><br> sabão em pó surf: 15,95R$'
+    sabaoemposuperrede.innerHTML = '<strong>Super Rede</strong><br>Último preço sabão em pó surf: 15,95R$'
 }
-function rank(){//Atualizar 12/05/2023
+function rank(){//Atualizar 
     rankmaisbarato.innerHTML = 'A classificação segue do mercado mais barato para o mais caro'
-    rankmaisbarato1.innerHTML = '1) Super Market com 4 produtos | Rio Sul com 4 produtos'
-    rankmaisbarato2.innerHTML = '2) Super Rede com 3 produtos'
-    rankmaisbarato3.innerHTML = '3) Rede Economia com 0 produtos'
-    rankmaisbarato4.innerHTML = ''
+    rankmaisbarato1.innerHTML = '1) Rede Economia com 5 produtos'
+    rankmaisbarato2.innerHTML = '2) Super Market com 4 produtos'
+    rankmaisbarato3.innerHTML = '3) Rio Sul com 3 produtos'
+    rankmaisbarato4.innerHTML = '4) Super Rede com 2 produtos'
 }
 function problema(){
     alert("Entrar em contato pelo e-mail: skilobaloba@gmail.com")
